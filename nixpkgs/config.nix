28d93cd393289cf440bfd18675573522f41a0d18
Mayeu <m@mayeu.me>
2018-06-17 13:47:14 +0200

    feat: add nixnote2 in my  package

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,13 +1,9 @@
 {
   allowUnfree = true;
   allowBroken = true;
-  #build-use-chroot = true;
-
-  #vim.python=true;
 
   packageOverrides = pkgs_ : with pkgs_; {
     my_neovim = import vim/customization.nix { inherit pkgs; };
-    #corebird = import ./corebird.nix {inherit pkgs; inherit stdenv;};
 
     all = with pkgs; buildEnv {
       name = "all";
@@ -16,6 +12,7 @@
         heroku
         ledger
         jitsi
+        nixnote2
       ];
     };
   };
