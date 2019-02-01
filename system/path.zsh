40b34ef684e48cd4ace4038f563eaf1ef1fb9d4b
Mayeu <m@mayeu.me>
2019-02-01 11:37:33 +0100

    feat(path): override the default mysql-client

--- system/path.zsh
+++ system/path.zsh
@@ -4,7 +4,7 @@
 #  - my local `bin` first
 #  - Homebrew specific override
 #  - yarn `bin` folder
-export PATH="${HOME}/bin":"/usr/local/opt/gnu-getopt/bin":"$HOME/.yarn/bin":$PATH
+export PATH="${HOME}/bin":"/usr/local/opt/mysql-client/bin":"/usr/local/opt/gnu-getopt/bin":"$HOME/.yarn/bin":$PATH
 
 # MANPATH
 #  - include homebrew man
