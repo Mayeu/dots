96cce3f3c21476411f8394371c0493179b768818
Mayeu <m@mayeu.me>
2019-03-08 11:52:55 +0700

    feat(path): override the default macOS make

--- system/path.zsh
+++ system/path.zsh
@@ -2,9 +2,10 @@
 
 # PATH
 #  - my local `bin` first
+#  - the GNU bin folder
 #  - Homebrew specific override
 #  - yarn `bin` folder
-export PATH="${HOME}/bin":"/usr/local/opt/mysql-client/bin":"/usr/local/opt/gnu-getopt/bin":"$HOME/.yarn/bin":$PATH
+export PATH="${HOME}/bin":"/usr/local/opt/mysql-client/bin":"/usr/local/opt/make/libexec/gnubin":"/usr/local/opt/gnu-getopt/bin":"$HOME/.yarn/bin":$PATH
 
 # MANPATH
 #  - include homebrew man
