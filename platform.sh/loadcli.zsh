3a12f7576075241de9ea9e9b78369edf4a8ce524
Mayeu <m@mayeu.me>
2019-03-23 10:04:52 +0700

    feat: add a configuration folder for platform.sh

---
+++ platform.sh/loadcli.zsh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# BEGIN SNIPPET: Platform.sh CLI configuration
+HOME=${HOME:-'/Users/m'}
+export PATH="$HOME/"'.platformsh/bin':"$PATH"
+if [ -f "$HOME/"'.platformsh/shell-config.rc' ]; then . "$HOME/"'.platformsh/shell-config.rc'; fi # END SNIPPET
