ad291de03013a49d359addbe42549ee5d69fcee8
Mayeu <m@mayeu.me>
2019-07-12 11:10:37 +0200

    refactor(bin): the bin folder is now automatically symlink'd

--- system/path.zsh
+++ system/path.zsh
@@ -5,7 +5,7 @@
 #  - the GNU bin folder
 #  - Homebrew specific override
 #  - yarn `bin` folder
-export PATH="${HOME}/bin":"/usr/local/opt/mysql-client/bin":"/usr/local/opt/make/libexec/gnubin":"/usr/local/opt/gnu-getopt/bin":"$HOME/.yarn/bin":$PATH
+export PATH="${HOME}/.bin":"/usr/local/opt/mysql-client/bin":"/usr/local/opt/make/libexec/gnubin":"/usr/local/opt/gnu-getopt/bin":"$HOME/.yarn/bin":$PATH
 
 # MANPATH
 #  - include homebrew man
