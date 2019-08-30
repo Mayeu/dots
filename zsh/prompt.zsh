3b92cd95bc548737a43edf9f0e1714e894a89247
Mayeu <m@mayeu.me>
2019-08-30 14:01:44 +0200

    feat(zsh): remove most of my prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,18 +1,17 @@
 #!/bin/zsh
 
 SPACESHIP_PROMPT_ORDER=(
-  user          # Username section
-  dir           # Current directory section
-  host          # Hostname section
-  git           # Git section (git_branch + git_status)
-  venv          # virtualenv section
-  conda         # conda virtualenv section
-  pyenv         # Pyenv section
-  terraform     # Terraform workspace section
-  exec_time     # Execution time
-  line_sep      # Line break
-  vi_mode       # Vi-mode indicator
-  jobs          # Background jobs indicator
-  exit_code     # Exit code section
-  char          # Prompt character
+  user # Username section
+  #  dir           # Current directory section
+  #  git           # Git section (git_branch + git_status)
+  venv # virtualenv section
+  conda # conda virtualenv section
+  pyenv # Pyenv section
+  terraform # Terraform workspace section
+  exec_time # Execution time
+  line_sep # Line break
+  vi_mode # Vi-mode indicator
+  jobs # Background jobs indicator
+  exit_code # Exit code section
+  char # Prompt character
 )
