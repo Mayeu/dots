2074e783190991308bf427e450ad0fe19b3d6952
Mayeu <m@mayeu.me>
2019-12-17 12:15:21 +0100

    chore: fix string double quoting for PATH

--- system/path.zsh
+++ system/path.zsh
@@ -9,9 +9,9 @@ export BIN_DIR="${HOME}/.bin"
 #  - the GNU bin folder
 #  - Homebrew specific override
 #  - yarn `bin` folder
-export PATH="${BIN_DIR}":"/usr/local/opt/mysql-client/bin":"/usr/local/opt/make/libexec/gnubin":"/usr/local/opt/gnu-getopt/bin":"$HOME/.yarn/bin":$PATH
+#  - the rest of the PATH
+export PATH="${BIN_DIR}:/usr/local/opt/mysql-client/bin:/usr/local/opt/make/libexec/gnubin:/usr/local/opt/gnu-getopt/bin:{$HOME}/.yarn/bin:$PATH"
 
 # MANPATH
 #  - include homebrew man
 export MANPATH="/usr/local/share/man:$MANPATH"
-
