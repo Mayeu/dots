c97a21238b944ae2a67437c57208b05855ca5af5
Mayeu <m@mayeu.me>
2020-05-17 18:56:15 +0200

    chore(doom): pin org-roam to a specific release instead of using doom's version

--- doom.d.symlink/packages.el
+++ doom.d.symlink/packages.el
@@ -45,7 +45,10 @@
 ;(package! builtin-package :recipe (:nonrecursive t))
 ;(package! builtin-package-2 :recipe (:repo "myfork/package"))
 
+
 ;; Specify a `:branch' to install a package from a particular branch or tag.
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
+
+(package! org-roam :recipe (:branch "v1.1.0"))
