f2ae26cd5ca9c54b827333b2d0365f3c6f4fc638
Mayeu <m@mayeu.me>
2020-11-05 12:44:55 +0100

    feat(gcloud): add gcloud configuration

---
+++ google-cloud-sdk/config.zsh
@@ -0,0 +1,5 @@
+#! /usr/bin/env zsh
+
+export CLOUDSDK_PYTHON="/usr/local/opt/python@3/libexec/bin/python"
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
