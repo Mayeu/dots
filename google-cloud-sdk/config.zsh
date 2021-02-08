c23055a6988b26da170142cf5bca325b1bb28687
Mayeu <m@mayeu.me>
2021-02-08 04:09:27 +0100

    chore(google): shitty not working completion is useless and only show warning

--- google-cloud-sdk/config.zsh
+++ google-cloud-sdk/config.zsh
@@ -1,5 +1,8 @@
 #! /usr/bin/env zsh
 
-export CLOUDSDK_PYTHON="/usr/local/opt/python@3/libexec/bin/python"
+export CLOUDSDK_PYTHON="/usr/local/opt/python@3.8/libexec/bin/python"
 source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+#source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+
+#gcloud zsh completion
+#fpath=(/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/ $fpath)
