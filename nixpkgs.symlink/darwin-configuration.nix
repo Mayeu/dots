43425ea507412b7b4a34972c6e915f45bc6df788
Mayeu <m@mayeu.me>
2021-05-08 19:51:59 +0200

    feat(nix): use unstable nix

--- nixpkgs.symlink/darwin-configuration.nix
+++ nixpkgs.symlink/darwin-configuration.nix
@@ -10,8 +10,14 @@
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
   # Auto upgrade nix package and the daemon service.
-  # services.nix-daemon.enable = true;
+  services.nix-daemon.enable = true;
   # nix.package = pkgs.nix;
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+   };
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
