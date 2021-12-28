49e1bf6d2def93d40312670a61c49b610d2818c5
Mayeu <m@mayeu.me>
2021-12-28 18:37:40 +0100

    chore(spaceship): reformat code

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,17 +2,18 @@
 
 SPACESHIP_PROMPT_ORDER=(
   begin # Just begin again section
-  user # Username section
+  user  # Username section
   #  dir           # Current directory section
-  git # Git section (git_branch + git_status)
-  venv # virtualenv section
-  conda # conda virtualenv section
-  pyenv # Pyenv section
+  git       # Git section (git_branch + git_status)
+  venv      # virtualenv section
+  conda     # conda virtualenv section
+  pyenv     # Pyenv section
   terraform # Terraform workspace section
   exec_time # Execution time
-  line_sep # Line break
-  vi_mode # Vi-mode indicator
-  jobs # Background jobs indicator
+  line_sep  # Line break
+  vi_mode   # Vi-mode indicator
+  #nix_shell # Nix shell indicator
+  jobs      # Background jobs indicator
   exit_code # Exit code section
-  char # Prompt character
+  char      # Prompt character
 )
