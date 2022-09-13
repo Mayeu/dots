6f75c1208480eb355c3468f738eadeca2c1a06df
Mayeu <m@mayeu.me>
2022-09-13 08:29:59 +0200

    chore(zsh/prompt): remove pyenv & vi_mode from my prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,11 +7,9 @@ SPACESHIP_PROMPT_ORDER=(
   git       # Git section (git_branch + git_status)
   venv      # virtualenv section
   conda     # conda virtualenv section
-  pyenv     # Pyenv section
   terraform # Terraform workspace section
   exec_time # Execution time
   line_sep  # Line break
-  vi_mode   # Vi-mode indicator
   #nix_shell # Nix shell indicator
   jobs      # Background jobs indicator
   exit_code # Exit code section
