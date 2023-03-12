f615a62806ce218d7c3c81c39aacf433c3f11a69
Mayeu <m@mayeu.me>
2023-03-12 11:42:04 +0100

    feat(make): configure make to use most of my CPU

--- make/config.zsh
+++ make/config.zsh
@@ -1,7 +1,7 @@
 #! /usr/bin/env zsh
 
-# Use all my CPU pliz"
-export MAKEFLAGS='-j 4 --no-print-directory' # -s'
+# Use most of my CPU pliz"
+export MAKEFLAGS='-j 8 --no-print-directory' # -s'
 
 # This is not for make, it is for makew
 export MAKE_PATH="/usr/local/bin/gmake"
