5335f594936905f91ad0d756278d368fe93cf27c
Mayeu <m@mayeu.me>
2023-07-02 18:16:53 +0200

    fix: std installation

--- src/apps/nvim.nix
+++ src/apps/nvim.nix
@@ -1,34 +1,38 @@
-{ config, pkgs, ... }:
-
+{
+  config,
+  pkgs,
+  ...
+}:
 pkgs.neovim.override {
-vimAlias = true;
-configure = {
-  packages.myPlugins = with pkgs.vimPlugins; {
-    start = [
-    lualine-nvim
-    (nvim-treesitter.withPlugins (
-          plugins: with plugins; [
-            nix
-            elixir
-            ruby
-            bash
-            dockerfile
-            erlang
-            gitignore
-            hcl
-            json
-            markdown
-            toml
-            yaml
-            lua
-          ]
+  vimAlias = true;
+  configure = {
+    packages.myPlugins = with pkgs.vimPlugins; {
+      start = [
+        lualine-nvim
+        (nvim-treesitter.withPlugins (
+          plugins:
+            with plugins; [
+              nix
+              elixir
+              ruby
+              bash
+              dockerfile
+              erlang
+              gitignore
+              hcl
+              json
+              markdown
+              toml
+              yaml
+              lua
+            ]
         ))
-  ];
-  opt = [];
-  };
+      ];
+      opt = [];
+    };
 
-  customRC = ''
-  lua require('lualine').setup()
-  '';
-};
+    customRC = ''
+      lua require('lualine').setup()
+    '';
+  };
 }
